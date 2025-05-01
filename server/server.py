56bcf6a7da96d3f23201fac1213c62a5f5302c80
Andrew Vagliano <andrew.vagliano1@gmail.com>
2025-05-01 12:01:10 -0400

    Move requirements.txt to top level; update server.py to handle direct runs

--- server/server.py
+++ server/server.py
@@ -13,4 +13,15 @@ def create_app():
 
     app.register_blueprint(api_bp)
 
+    app.add_url_rule("/", "root", lambda: "Hello, World!")
+
     return app
+
+
+if __name__ == "__main__":
+    from waitress import serve
+
+    host, port = "0.0.0.0", 5001
+
+    print(f"Serving app on http://{host}:{port}")
+    serve(create_app(), host=host, port=port)
